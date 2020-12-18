702e8817f31cbe23726501de7885b30cd8a800a4
lcsious <lcsious@gmail.com>
2020-12-19 00:50:45 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import discord #discordでBOTを使うのにこれが必ずいる
+
+client = discord.Client()
+
+@client.event #BOT起動時にCMDに表示される部分で無くてもよい
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message): #メッセージを受け取る関数なので必ず必要
+    if message.content == "おはよう":
+    #:(コロン)を忘れずつけよう！Enterを押すと自動で改行されるよ！
+        await client.send_message(message.channel, "Hello world!!")
+
+# この＊＊＊に自分のトークンを書き替える
+client.run('*******************')
