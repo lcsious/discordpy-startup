3f9c2b382f1bfaba5f26f69250551669fe55502d
lcsious <lcsious@gmail.com>
2020-12-19 00:51:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import discord #discordでBOTを使うのにこれが必ずいる
 
 client = discord.Client()
+token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event #BOT起動時にCMDに表示される部分で無くてもよい
 async def on_ready():
@@ -16,4 +17,4 @@ async def on_message(message): #メッセージを受け取る関数なので必
         await client.send_message(message.channel, "Hello world!!")
 
 # この＊＊＊に自分のトークンを書き替える
-client.run('*******************')
+client.run(token)
