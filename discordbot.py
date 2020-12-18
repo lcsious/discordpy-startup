f493aa0f1719188a8d89155d1d4f709539cedf2d
lcsious <lcsious@gmail.com>
2020-12-19 00:32:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,8 +17,13 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pongg')
 
-@bot.command()
-async def paon(ctx):
-    await ctx.send('pien')
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == 'ねこ':
+        await message.channel.send('にゃーん')
 
 bot.run(token)
