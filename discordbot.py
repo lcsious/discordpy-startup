a7b397885a1a8187e84a6e934282e41839ad9206
lcsious <lcsious@gmail.com>
2020-12-19 00:56:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
-import discord
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -18,11 +16,24 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pongg')
 
+bot.run(token)
+
+import discord #discordでBOTを使うのにこれが必ずいる
+
+client = discord.Client()
+
+@client.event #BOT起動時にCMDに表示される部分で無くてもよい
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
 @client.event
 async def on_message(message): #メッセージを受け取る関数なので必ず必要
     if message.content == "おはよう":
     #:(コロン)を忘れずつけよう！Enterを押すと自動で改行されるよ！
-        await client.send_message(message.channel, "Hello world!!") 
+        await client.send_message(message.channel, "Hello world!!")
 
-bot.run(token)
+# この＊＊＊に自分のトークンを書き替える
 client.run(token)
