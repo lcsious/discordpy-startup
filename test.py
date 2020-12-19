93ec764086bf0a991eda980644c58288d86f07c8
lcsious <lcsious@gmail.com>
2020-12-19 10:26:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,6 @@ import os
 client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event #BOT起動時にCMDに表示される部分で無くてもよい
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
 @client.event
 async def on_message(message):
     # 「おはよう」で始まるか調べる
@@ -18,9 +11,10 @@ async def on_message(message):
         # 送り主がBotだった場合反応したくないので
         if client.user != message.author:
             # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
+            m = "おはようございます" + message.author.nick + "さん！"
             # メッセージが送られてきたチャンネルへメッセージを送ります
             await message.channel.send(m)
 
-# この＊＊＊に自分のトークンを書き替える
+
+# 実行
 client.run(token)
